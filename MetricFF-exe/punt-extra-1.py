c4f7af8f4793cece79acd58e198c6fefb55e7b91
CarlesOrriols <carles.orriols@estudiantat.upc.edu>
2023-01-05 19:19:17 +0100

    python

--- MetricFF-exe/punt-extra-1.py
+++ MetricFF-exe/punt-extra-1.py
@@ -3,24 +3,25 @@ import sys
 
 def main():
 
-    if (len(sys.argv)>1):
-        problem_file = sys.argv[1]
-    else:
-        problem_file = "prob_ext2_v2.pddl"
-
     if (len(sys.argv)>2):
-        domain_file = sys.argv[2]
+        problem_file = sys.argv[2]
     else:
-        # domain_file = "domini_ext2.pddl"
-        domain_file = "domini_punt_extra_1.pddl"
+        # problem_file = "prob_ext2_v2.pddl"
+        problem_file = "prob_punt_extra_1.pddl"
 
-    escriureProblema(domain_file)
+
+    if (len(sys.argv)>1):
+        domain_file = sys.argv[1]
+    else:
+        domain_file = "domini_ext2.pddl"
+
+    # escriureProblema(problem_file)
     executarProblemaPDDL(domain_file, problem_file)
     # print("executant punt extra 1")
 
 
-def escriureProblema(domain_file, ):
-    f = open(domain_file, "w")
+def escriureProblema(problem_file):
+    f = open(problem_file, "w")
     f.write("Now the file has more content!")
     f.close()
 
